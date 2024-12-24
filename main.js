c31f01d58dd4088a67aadb271bea5af74ac7fadd
emmaheftman <emma.h@tech-tav.com>
2024-12-24 14:35:00 +0200

    changed function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function onBallClick(elBall){
-    elBall.style.width= '50px'
-    elBall.style.height= '50px'
-    elBall.innerHTML = '50px * 50px'
+    elBall.style.width +=50
+    elBall.style.height += 50
+    elBall.innerHTML = "",elBall.style.height,elBall.style.width
 }
