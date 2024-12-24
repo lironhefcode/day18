530904459c9eb369c22671b36f7883f08fae45f3
emmaheftman <emma.h@tech-tav.com>
2024-12-24 14:21:01 +0200

    add function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-function onBallClick(){
-    console.log('hello')
+function onBallClick(elBall){
+    elBall.style.width= '50px'
+    elBall.style.height= '50px'
+    elBall.innerHTML = '50px * 50px'
 }
