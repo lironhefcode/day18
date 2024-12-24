5723c20fc79fc7c352ba9e208327a628cefb3a84
lironhefcode <lironheftman@gmail.com>
2024-12-24 17:17:19 +0200

    added ball max size

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function onBallClick(elBall){
+function onBallClick(elBall,maxDiameter){
     elBall.style.width  = (parseInt(elBall.style.width)||100) +getRandomInt(20,60) + 'px'
     elBall.style.height = (parseInt(elBall.style.height)||100)+getRandomInt(20,60) + 'px'
-    if(parseInt(elBall.style.height)>400){
+    if(parseInt(elBall.style.height)>maxDiameter){
         elBall.style.width = '100px'
         elBall.style.height = '100px'
     }
