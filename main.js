9c13355ea466f9b0b135697dda0937d85312e3e3
liron heftman <lironheftman@gmail.com>
2024-12-24 15:47:49 +0200

    function change

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function onBallClick(elBall){
     elBall.style.width  = (parseInt(elBall.style.width)||100) +50 + 'px'
     elBall.style.height = (parseInt(elBall.style.height)||100)+50 + 'px'
+    if(parseInt(elBall.style.height)>400){
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+    }
     elBall.innerHTML = elBall.style.height , elBall.style.height
 }
