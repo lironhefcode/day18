37dddcab043e9cfe728fb93243920e436ea17b1a
lironhefcode <lironheftman@gmail.com>
2024-12-24 16:51:53 +0200

    ball size increase by random number

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function onBallClick(elBall){
-    elBall.style.width  = (parseInt(elBall.style.width)||100) +50 + 'px'
-    elBall.style.height = (parseInt(elBall.style.height)||100)+50 + 'px'
+    elBall.style.width  = (parseInt(elBall.style.width)||100) +getRandomInt(20,60) + 'px'
+    elBall.style.height = (parseInt(elBall.style.height)||100)+getRandomInt(20,60) + 'px'
     if(parseInt(elBall.style.height)>400){
         elBall.style.width = '100px'
         elBall.style.height = '100px'
